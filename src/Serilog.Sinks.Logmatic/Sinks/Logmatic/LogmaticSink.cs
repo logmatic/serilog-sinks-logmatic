0f539a4d7f87c034aa24f59908cd8001d1aa47b6
gpolaert <gpolaert@DESKTOP-VPGODUK>
2016-09-01 17:34:22 +0200

    Thread-safe improvement, close #1

---
+++ src/Serilog.Sinks.Logmatic/Sinks/Logmatic/LogmaticSink.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Serilog.Events;
+using System.IO;
+using System.Text;
+using Serilog.Formatting.Json;
+using Serilog.Sinks.PeriodicBatching;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Serilog.Sinks.Logmatic
+{
+    public class LogmaticSink : PeriodicBatchingSink
+    {
+
+        private readonly string _token;
+        private readonly JsonFormatter _formatter = new JsonFormatter();
+        private LogmaticClient _client;
+        private readonly LogmaticClientConfiguration _clientConfig;
+
+
+        public LogmaticSink(string token, LogmaticClientConfiguration clientConfiguration, TimeSpan period, int batchSizeLimit) : base(batchSizeLimit, period)
+        {
+            _token = token;
+            _clientConfig = clientConfiguration;
+        }
+
+
+        protected override void EmitBatch(IEnumerable<LogEvent> events)
+        {
+
+            if (!events.Any()) return;
+
+            lock (this)
+            {
+                if (_client == null)
+                {
+                    // connect or reconnect to the endpoint
+                    _client = new LogmaticClient(_clientConfig);
+                }
+            }
+
+            // format the event
+            var payload = new StringBuilder();
+            var sw = new StringWriter(payload);
+
+            foreach (var logEvent in events)
+            {
+                payload.Append(_token).Append(" ");
+                _formatter.Format(logEvent, sw);
+            }
+            // send the event
+            _client.writeAndRetry(payload.ToString());
+
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (_client != null)
+            {
+                _client.flush();
+                _client.close();
+                _client = null;
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
