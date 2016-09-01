40a975735d12caab700225020acf0558bdf6355d
gpolaert <gpolaert@DESKTOP-VPGODUK>
2016-09-01 17:35:12 +0200

    draft v2

---
+++ src/Serilog.Sinks.Logmatic/Sinks/Logmatic/LogmaticClient.cs
@@ -0,0 +1,149 @@
+﻿using Serilog.Debugging;
+using System;
+using System.IO;
+using System.Net.Security;
+using System.Net.Sockets;
+using System.Threading;
+using System.Text;
+
+namespace Serilog.Sinks.Logmatic
+{
+    public class LogmaticClient
+    {
+        private LogmaticClientConfiguration _clientConfig;
+        private TcpClient _client;
+
+        private SslStream _sslStream;
+        private Stream _rawStream;
+        // Simple wrapper depending of the connection type (ssl or not)
+        private Stream _stream
+        {
+            get
+            {
+                return _clientConfig.UseSSL ? _sslStream : _rawStream;
+            }
+        }
+
+
+        private static readonly UTF8Encoding UTF8 = new UTF8Encoding();
+
+        
+        public LogmaticClient(LogmaticClientConfiguration clientConfig)
+        {
+            _clientConfig = clientConfig;
+
+        }
+
+        // connect to the specify endpoint
+        private void connect()
+        {
+            SelfLog.WriteLine("Starting a new connection to {0}:{1} (SSL = {2})", _clientConfig.Ip, _clientConfig.Port, _clientConfig.UseSSL);
+            try
+            {
+                _client = new TcpClient();
+                _client.ConnectAsync(_clientConfig.Ip, _clientConfig.Port).Wait();
+
+                _rawStream = _client.GetStream();
+
+
+                if (_clientConfig.UseSSL)
+                {
+                    _sslStream = new SslStream(_rawStream);
+                    _sslStream.AuthenticateAsClientAsync(_clientConfig.Ip).Wait();
+                }
+            }
+            catch (Exception e)
+            {
+                throw new Exception(e.Message);
+            }
+        }
+
+
+        public void writeAndRetry(string payload)
+        {
+
+            for (int i = 0; i < _clientConfig.MaxRetries; i++)
+            {
+
+                // backoff mechanism
+                int backoffTime = (int)Math.Min(Math.Pow(i, 2), _clientConfig.MaxBackoff);
+                if (backoffTime > 0)
+                {
+                    SelfLog.WriteLine("Making a new attempt in {0} seconds ({1}/{2})", backoffTime, i, _clientConfig.MaxRetries);
+                }
+                Thread.Sleep(backoffTime * 1000);
+
+
+                if (_client == null || _client.Connected == false)
+                {
+                    try
+                    {
+                        connect();
+                    }
+                    catch (Exception e)
+                    {
+                        SelfLog.WriteLine("Exception while connecting client: {0}", e);
+                        continue;
+                    }
+                }
+
+                try
+                {
+                    byte[] data = UTF8.GetBytes(payload);
+                    _stream.Write(data, 0, data.Length);
+                    return;
+                }
+                catch (Exception e)
+                {
+                    SelfLog.WriteLine("Retry to send log event: {0}", e);
+                }
+
+            }
+            SelfLog.WriteLine("Exception while sending log event, event dropped");
+
+        }
+
+
+        public void flush()
+        {
+            if (_stream != null)
+            {
+                try
+                {
+                    _stream.Flush();
+                }
+                catch (Exception e)
+                {
+                    SelfLog.WriteLine("Exception while flushing client: {0}", e);
+                }
+            }
+        }
+
+        public void close()
+        {
+            if (_client != null)
+            {
+                try
+                {
+                    flush();
+
+#if NETSTANDARD1_4
+                    _client.Dispose();
+#else
+                    _client.Close();
+#endif
+                    _client = null;
+                }
+                catch (Exception e)
+                {
+                    SelfLog.WriteLine("Exception while closing client: {0}", e);
+                }
+            }
+        }
+
+
+
+    }
+
+
+}
---
+++ src/Serilog.Sinks.Logmatic/Sinks/Logmatic/LogmaticClientConfiguration.cs
@@ -0,0 +1,30 @@
+﻿namespace Serilog.Sinks.Logmatic
+{
+    public class LogmaticClientConfiguration
+    {
+
+        private const string DEFAULT_LOGMATIC_IP = "api.logmatic.io";
+        private const int DEFAULT_LOGMATIC_PORT = 10514;
+        private const int DEFAULT_LOGMATIC_SSL_PORT = 10515;
+
+
+        public string Ip { get; }
+        public int Port { get; }
+        public bool UseSSL { get; }
+        public int MaxRetries { get; set; } = 10;
+        public byte MaxBackoff { get; set; } = 30;
+
+
+        public LogmaticClientConfiguration(string ip = null, int port = 0, bool useSSL = true)
+        {
+            // setting-up defaults
+            Ip = (ip == null) ? DEFAULT_LOGMATIC_IP : ip;
+            UseSSL = useSSL;
+            Port = port;
+            if (Port == 0)
+            {
+                Port = (useSSL == true) ? DEFAULT_LOGMATIC_SSL_PORT : DEFAULT_LOGMATIC_PORT;
+            }
+        }
+    }
+}
