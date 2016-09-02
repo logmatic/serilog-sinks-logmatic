4afeb37152732a8b56d825326b85bebe7eb5966f
gpolaert <gpolaert@DESKTOP-VPGODUK>
2016-09-02 11:56:20 +0200

    packaging information

--- src/Serilog.Sinks.Logmatic/Properties/AssemblyInfo.cs
+++ src/Serilog.Sinks.Logmatic/Properties/AssemblyInfo.cs
@@ -4,3 +4,9 @@
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyProduct("Serilog.Sinks.Logmatic")]
+[assembly: AssemblyTitle("Serilog.Sinks.Logmatic")]
+[assembly: AssemblyDescription("Serilog sink for Logmatic.io")]
+[assembly: AssemblyCopyright("Copyright © Serilog Contributors 2016, Logmatic.io")]
+
+[assembly: AssemblyVersion("0.5.0.0")]
+
